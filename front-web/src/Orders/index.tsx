28c9e53db8a6a206d8b864a17d0e3b5dd15b8113
felipecarvalhogodoi98 <felipecarvalhogodoi98@gmail.com>
2021-01-07 00:21:29 -0300

    Add db conection

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Product } from './types'
+import ProductsList from './ProductsList'
 import StepesHeader from './StepsHeader'
 import './styles.css'
+import { fetchProducts } from '../api'
 
 function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+    
+    useEffect(
+        () => {
+            fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+        },
+        []
+    );
+
     return (
         <div className="orders-container">
             <StepesHeader/>
+            <ProductsList products = {products}/>
         </div>
     )
 }
