c97ba667c8e87622249c61d7da84175aa0aec23f
felipecarvalhogodoi98 <felipecarvalhogodoi98@gmail.com>
2021-01-09 15:29:57 -0300

    Finish Front Web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,18 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 import Routes from './Routes';
 
 
 function App() {
   return (
+    <>
     <div>
       <Routes/>
+      <ToastContainer/>
     </div>
+    </>
   );
 }
 
